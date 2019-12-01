29eb899a2826aac0df98cba4dbf52e9a3e9f1402
Ben Crane <bencrane5.12@gmail.com>
2019-12-01 14:23:36 -0500

    Update database connection

--- routes/api/collections.js
+++ routes/api/collections.js
@@ -2,15 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
+const { testCollections } = require('../../lib/queries');
+
 // @route GET /api/collections/test
 // @desc Test new route for server deploys
 // @access Public
 router.get('/test', async(req, res) => {
     console.log('Test route hit');
-    res.json({
-        success: true,
-        message: 'Test route hit'
-    });
+    const result = await testCollections();
+    if (result.success) {
+        res.json({
+            success: true,
+            message: result.data
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'An error occurred'
+        });
+    }
 });
 
 module.exports = router;
