21fe83fb93a7bea382a9b5620df1cd5a1e0272ce
Ben Crane <bencrane5.12@gmail.com>
2019-12-02 10:09:01 -0500

    Add get users collections route

--- routes/api/collections.js
+++ routes/api/collections.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-const { testCollections, createCollection } = require('../../lib/queries');
+const { testCollections, createCollection, getUserCollections } = require('../../lib/queries');
 
 // @route GET /api/collections/test
 // @desc Test new route for server deploys
-// @access Public
+// @access Authenticated
 router.get('/test', async(req, res) => {
     console.log('Updated test route hit');
     const result = await testCollections();
@@ -23,6 +23,9 @@ router.get('/test', async(req, res) => {
     }
 });
 
+// @route POST /api/collections/create
+// @desc Route to create new collection
+// @access Authenticated
 router.post('/create', async(req, res) => {
     console.log('Create route hit');
     const values = req.body;
@@ -33,6 +36,31 @@ router.post('/create', async(req, res) => {
             success: true,
             message: result.data
         })
+    }
+    res.json({
+        success: false,
+        message: result.error
+    });
+});
+
+// @route GET /api/collections/user/:userID
+// @desc Get the collections for a user
+// @access Authenticated
+router.get('/user/:userID', async(req, res) => {
+    console.log('Collection route hit');
+    const userID = req.params.userID;
+    if (userID != req.user.sub) {
+        res.json({
+            success: false,
+            message: 'Invalid user'
+        });
+    }
+    const result = await getUserCollections(userID);
+    if (result.success) {
+        res.json({
+            success: true,
+            message: result.data
+        });
     }
     res.json({
         success: false,
