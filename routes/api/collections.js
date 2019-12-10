123c1d6720168f66fc8e276d80beb89d377d2e27
Ben Crane <bencrane5.12@gmail.com>
2019-12-09 21:06:54 -0500

    Collection route refactoring

--- routes/api/collections.js
+++ routes/api/collections.js
@@ -7,21 +7,33 @@ const { createCollection, getUserCollections } = require('../../lib/queries');
 // @route POST /api/collections/create
 // @desc Route to create new collection
 // @access Authenticated
+// TODO: this try/catch is clunky; look up better ways to handle await calls (should have no errors logged though)
 router.post('/create', async(req, res) => {
-    console.log('Create route hit');
-    const values = req.body;
-    values.owner = req.user.sub;
-    const result = await createCollection(values);
-    if (result.success) {
+    console.log('Create route hit');    // TODO: remove this line when function is finished
+    const values = {
+        ...req.body,
+        owner: req.user.sub
+    };
+    try {
+        const result = await createCollection(values);
+        if (result.success) {
+            res.json({
+                success: true,
+                message: result.data
+            });
+        } else {
+            res.json({
+                success: true,
+                message: result.error
+            })
+        }
+    } catch(err) {
+        // TODO: implement utility function that sends res.json with whatever json payload (data and errors should be separate)
         res.json({
-            success: true,
-            message: result.data
+            success: false,
+            message: err
         });
     }
-    res.json({
-        success: false,
-        message: result.error
-    });
 });
 
 // @route GET /api/collections/user/:userID
